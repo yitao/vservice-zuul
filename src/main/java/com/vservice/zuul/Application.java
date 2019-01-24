454663caeb194dd4bb89ac7d9596593cafd43660
yitao <yitao@vibeac.com>
2019-01-24 16:57:14 +0800

    修改eureka注册配置

--- src/main/java/com/vservice/zuul/Application.java
+++ src/main/java/com/vservice/zuul/Application.java
@@ -3,8 +3,6 @@ package com.vservice.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -14,8 +12,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
-@EnableHystrix
-@EnableHystrixDashboard
+//@EnableHystrix
+//@EnableHystrixDashboard
 public class Application {
 
     public static void main(String[] args) {
